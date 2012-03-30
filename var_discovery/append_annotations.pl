62e63678191e1810640a1111c18fe9abde191ab3
Adam Cornish <adam.cornish@unmc.edu>
2012-03-30 17:12:58 -0500

    getting close to doing the dbNSFP annotations

--- var_discovery/append_annotations.pl
+++ var_discovery/append_annotations.pl
@@ -4,49 +4,39 @@ use strict;
 
 my @list = `cat $ARGV[0] | grep -v '^#'`;
 my $curr = "";
-my @db;
-open OUT, ">wat";
+open OUT, ">y";
+open DDL, ">z";
+my ($result, $prev_search) = ("","");
 foreach my $item ( @list )
 {
     my ($chrom) = $item =~ /^(\S+)/;
-    my @split  = split "\t", $item;
-    if ( $curr ne $chrom )
+    my @split   = split "\t", $item;
+    my ($eff)   = $item =~ /EFF=(.+?)\s/;
+    my @effects = split ",", $eff;
+    my $db      = "/data/genomes/Homo_sapiens/UCSC/hg19/Annotation/Variation/dbNSFP/$chrom";
+
+    foreach my $effect ( @effects )
     {
-        @db = `cat /data/genomes/Homo_sapiens/UCSC/hg19/Annotation/Variation/dbNSFP/chr$chrom`;
-        $curr = $chrom;
-    }
-    if ( $item =~ /NON_SYN/ )
-    {
-        my ($chr)  = $split[0] =~ /chr(\S+)/;
-        my $search = "$chr\\s\\S+\\s$split[3]\\s$split[4]\\s\\S\\s\\S\\s$split[1]";
-        print "search  : $search\n";
-    }
-    else
-    {
-        my ($eff)   = $item =~ /EFF=(.+?)\s/;
-        my @effects = split ",", $eff;
-        foreach my $effect ( @effects )
+        my ($loc, $parens) = $effect =~ /(.+?)\((.+?)\)/;
+        my ($impact,$type,$codon,$aa,$gene,$rna,$coding,$acc,$exon) = split /\|/, $parens;
+        if ( $loc =~ /NON_SYNON/ )
         {
-            my ($loc, $parens) = $effect =~ /(.+?)\((.+?)\)/;
-            my ($impact,$type,$codon,$aa,$gene,$rna,$coding,$acc,$exon) = split /\|/, $parens;
-            print "$split[0]\t$split[1]\t$split[2]\t$split[3]\t$split[4]\t$loc\t$impact\t$type\t$codon\t$aa\t$gene\t$acc\t$exon\t\n";
+            my ($ref_AA, $new_AA) = $aa =~ /([A-Z])\d+([A-Z])/;
+            my $search = "$split[3]\\s$split[4]\\s$ref_AA\\s$new_AA\\s$split[1]";
+            if ( $search ne $prev_search )
+            {
+                $result = `grep -P '$search' $db`;
+                $prev_search = $search;
+            }
+            print "eff: $effect\n\t\t$result\n";
+            print DDL "$result\n";
+        }
+        else
+        {
             print OUT "$split[0]\t$split[1]\t$split[2]\t$split[3]\t$split[4]\t$loc\t$impact\t$type\t$codon\t$aa\t$gene\t$acc\t$exon\t\n";
         }
+        print "prev_search: $prev_search\n";
     }
 }
-
-sub bin_search
-{
-    my ( $elem, $list ) = @_;
-    my $max = $#$list;
-    my $min = 0;
-
-    while ( $max >= $min ) {
-        my $mid    = int( ( $max + $min ) / 2 );
-        my ($item) = $list[$mid] =~ /\S+\s\S+\s\S\s\S\s\S\s\S\s(\S+)/;
-        if    ( $list->[$mid] < $elem ) { $min = $mid + 1; }
-        elsif ( $list->[$mid] > $elem ) { $max = $mid - 1; }
-        else                              { return $mid; }
-    }
-    return;
-}
+close OUT;
+close DDL;
