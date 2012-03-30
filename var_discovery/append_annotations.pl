52c8e26573cb7c5ab2c04a17769e88931efe5cca
Adam Cornish <adam.cornish@unmc.edu>
2012-03-30 16:01:58 -0500

    still not working

--- var_discovery/append_annotations.pl
+++ var_discovery/append_annotations.pl
@@ -3,20 +3,51 @@ use warnings;
 use strict;
 
 my @list = `cat $ARGV[0] | grep -v '^#'`;
-my $curr = 0;
+my $curr = "";
 my @db;
+open OUT, ">wat";
 foreach my $item ( @list )
 {
     my ($chrom) = $item =~ /^(\S+)/;
-    $curr = $chrom unless $curr;
+    my @split  = split "\t", $item;
+    if ( $curr ne $chrom )
+    {
+        @db = `cat /data/genomes/Homo_sapiens/UCSC/hg19/Annotation/Variation/dbNSFP/chr$chrom`;
+        $curr = $chrom;
+    }
     if ( $item =~ /NON_SYN/ )
     {
-        my @split  = split "\t", $item;
         my ($chr)  = $split[0] =~ /chr(\S+)/;
-        my $chunk  = `grep $split[1] /data/genomes/Homo_sapiens/UCSC/hg19/Annotation/Variation/dbNSFP/chr$chr`;
         my $search = "$chr\\s\\S+\\s$split[3]\\s$split[4]\\s\\S\\s\\S\\s$split[1]";
         print "search  : $search\n";
-        my ($line) = $chunk =~ /($search.+?)\n/;
-        print "line is : $line\n";
     }
+    else
+    {
+        my ($eff)   = $item =~ /EFF=(.+?)\s/;
+        my @effects = split ",", $eff;
+        foreach my $effect ( @effects )
+        {
+            my ($loc, $parens) = $effect =~ /(.+?)\((.+?)\)/;
+            my ($impact,$type,$codon,$aa,$gene,$rna,$coding,$acc,$exon) = split "|", $parens;
+            print "$split[0]\t$split[1]\t$split[2]\t$split[3]\t$split[4]\t$loc\t$impact\t$type\t$codon\t$aa\t$gene\t$acc\t$exon\t";
+            <>;
+            print OUT "$split[0]\t$split[1]\t$split[2]\t$split[3]\t$split[4]\t$loc\t$impact\t$type\t$codon\t$aa\t$gene\t$acc\t$exon\t";
+        }
+    }
+}
+
+sub bin_search
+{
+    my ( $elem, $list ) = @_;
+    my $max = $#$list;
+    my $min = 0;
+
+    while ( $max >= $min ) {
+        my $mid    = int( ( $max + $min ) / 2 );
+        my ($item) = $list[$mid] =~ /\S+\s\S+\s\S\s\S\s\S\s\S\s(\S+)/;
+        if    ( $list->[$mid] < $elem ) { $min = $mid + 1; }
+        elsif ( $list->[$mid] > $elem ) { $max = $mid - 1; }
+        else                              { return $mid; }
+    }
+    return;
 }
