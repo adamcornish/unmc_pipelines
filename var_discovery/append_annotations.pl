0fa0f73e19e3e8999b41f310788228c360ff6ddb
Adam Cornish <adam.cornish@unmc.edu>
2012-04-03 14:52:15 -0500

    add support for doing multiple files

--- var_discovery/append_annotations.pl
+++ var_discovery/append_annotations.pl
@@ -2,46 +2,55 @@
 use warnings;
 use strict;
 
-my @db   = "";
-my @data = `cat $ARGV[0] | grep -v '^#'`;
-my $curr = "";
-open OUT, ">ddl";
+print "Usage: perl $0 directory.\n\tSimply provide the directory that has the *finished.snvs* vcf files in them.\n\n";
+my $dir = ( $ARGV[0] ) ? $ARGV[0] : "./";
+chomp ( my @files = `ls *finished.snvs*` );
 
-foreach my $datum ( @data )
+foreach my $file ( @files )
 {
-    my ($chr)   = $datum =~ /^(\S+)/;
-    if ( $curr ne $chr )
-    {
-        print "Reading in $chr.\n";
-        @db = `cat /data/genomes/Homo_sapiens/UCSC/hg19/Annotation/Variation/dbNSFP/$chr | grep -vP '^#'`;
-        print "Done reading the db.\n";
-        $curr = $chr;
-    }
-    my @split   = split "\t", $datum;
-    my ($eff)   = $datum =~ /EFF=(.+?)\s/;
-    my @effects = split ",", $eff;
-    foreach my $effect ( @effects )
-    {
-        my ($loc, $parens) = $effect =~ /(.+?)\((.+?)\)/;
-        my ($impact,$type,$codon,$aa,$gene,$rna,$coding,$acc,$exon) = split /\|/, $parens;
-        print OUT "$split[0]\t$split[1]\t$split[2]\t$split[3]\t$split[4]\t$loc\t$impact\t$type\t$codon\t$aa\t$gene\t$acc\t$exon";
-        if ( $effect =~ /NON_SYN/)
-        {
-            my ($ref_AA, $alt_AA) = $aa =~ /([A-Z])\d+([A-Z])/;
-            my ($ref_nuc, $alt_nuc, $index) = ($split[3], $split[4], $split[1]);
-            my $result = bin_search ( \@split, \@db, $ref_AA, $alt_AA );
-            if ( $result )
-            {
-                my @col = split "\t", $result;
-                my $out = "\t$col[7]\t$col[8]\t$col[9]\t$col[10]\t$col[11]\t$col[12]\t$col[13]";
-                print OUT $out;
-            }
-        }
-        print OUT "\n";
-    }
-}
+    print "Working on $file\n";
+    my @db   = "";
+    my @data = `cat $file | grep -v '^#'`;
+    my $curr = "";
+    my ($name ) = $file =~ /(.+?)\./;
+    open OUT, ">$name.snvs.txt";
+    print OUT "Chromosome\tPosition\tdbSNP ID\tRef\tAlt\tMutation\tImpact\tType\tCodon Change\tAA Change\tGene\tAccession ID\tExon\tPhyloP\tSIFT\tPolyphen2\tLRT\tMutationTaster\n";
 
-close OUT;
+    foreach my $datum ( @data )
+    {
+        my ($chr)   = $datum =~ /^(\S+)/;
+        if ( $curr ne $chr )
+        {
+            print "\tReading in $chr.\n";
+            @db = `cat /data/genomes/Homo_sapiens/UCSC/hg19/Annotation/Variation/dbNSFP/$chr | grep -vP '^#'`;
+            $curr = $chr;
+        }
+        my @split   = split "\t", $datum;
+        my ($eff)   = $datum =~ /EFF=(.+?)\s/;
+        my @effects = split ",", $eff;
+        foreach my $effect ( @effects )
+        {
+            my ($loc, $parens) = $effect =~ /(.+?)\((.+?)\)/;
+            my ($impact,$type,$codon,$aa,$gene,$rna,$coding,$acc,$exon) = split /\|/, $parens;
+            print OUT "$split[0]\t$split[1]\t$split[2]\t$split[3]\t$split[4]\t$loc\t$impact\t$type\t$codon\t$aa\t$gene\t$acc\t$exon";
+            if ( $effect =~ /NON_SYN/)
+            {
+                my ($ref_AA, $alt_AA) = $aa =~ /([A-Z])\d+([A-Z])/;
+                my ($ref_nuc, $alt_nuc, $index) = ($split[3], $split[4], $split[1]);
+                my $result = bin_search ( \@split, \@db, $ref_AA, $alt_AA );
+                if ( $result )
+                {
+                    my @col = split "\t", $result;
+                    my $out = "\t$col[7]\t$col[8]\t$col[9]\t$col[10]\t$col[12]\n";
+                    print OUT $out;
+                }
+            }
+            print OUT "\n";
+        }
+    }
+
+    close OUT;
+}
 
 sub bin_search
 {
